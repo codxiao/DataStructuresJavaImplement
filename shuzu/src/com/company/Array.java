09e439205da5e2dc63a38ce376d30500f3de7a2f
codxiao <15551120617@163.com>
2018-07-24 11:01:09 +0800

    完成自己数组的方法定义，包括添加方法，删除方法，查找方法。并完成测试。

--- shuzu/src/com/company/Array.java
+++ shuzu/src/com/company/Array.java
@@ -25,5 +25,89 @@ public class Array {
     public boolean isEmpty(){
         return size==0;
     }
+    //在数组末尾添加元素
+    public void addAtLast(int e){
+             addAtMiddle(size,e);                                                                                       /*
+                                                                                                    if(size==data.length){
+             原始逻辑在右侧                                                                                           throw new IllegalArgumentException("数组已满，无法插入");
+                                                                                                    }
+                                                                                                    data[size]=e;
+                                                                                                    size++;
+                                                                                                    */
+    }
+    public void addAtFirst(int e){
+        addAtMiddle(0,e);
+    }
+    //在数组的数据中间插入一个元素
+    public void addAtMiddle(int index,int e){
+        if(size==data.length){
+            throw new IllegalArgumentException("数组满，无法插入");
+        }
+        if (index<0||index>size)
+            throw new IllegalArgumentException("index参数错误，无法插入");
+        //方法1
+        int l=size;
+        while (index<l){
+            data[l]=data[l-1];
+            l--;
+        }
+        data[index]=e;
+        size++;                                                  /*
+                                                            方法二
+                                                            for(int i=size;i>index;i--){
+                                                                data[size]=data[size-1];
 
+                                                            }
+                                                            */
+
+
+        }
+        //是否包含一个元素
+        public boolean contains(int e){
+            for (int i=0;i<size;i++){
+                if(data[i]==e)
+                    return true;
+            }
+            return false;
+        }
+        //查找一个元素
+    public int findElement(int e){
+        for (int i=0;i<size;i++){
+            if(data[i]==e)
+                return i;
+        }
+        return -1;
+    }
+    //删除一个元素
+    public int deleteByIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("索引不合法");
+        }
+        int result=data[index];
+        for (int i=index+1;i<=size-1;i++){
+            data[i-1]=data[i];
+        }
+        size--;
+        return result;
+    }
+    public void deleteByElement(int e){
+       int local=findElement(e);
+       if (local==-1){
+           throw new IllegalArgumentException("删除的元素不存在！");
+       }
+       for (int i=local+1;i<=size-1;i++){
+           data[i-1]=data[i];
+       }
+       size--;
+    }
+    @Override
+    public String toString(){
+        StringBuilder result=new StringBuilder();
+        for(int i=0;i<size;i++){
+            result.append(data[i]);
+            if (i!=size-1)
+            result.append(',');
+        }
+        return "数组大小："+size+"     元素为"+result;
+    }
 }
--- shuzu/src/com/company/Main.java
+++ shuzu/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Array myArray=new Array(10);
+	myArray.addAtLast(1);
+	myArray.addAtLast(2);
+	myArray.addAtLast(3);
+	myArray.addAtLast(4);
+	System.out.println(myArray.toString());
+	myArray.addAtLast(5);
+	System.out.println(myArray.toString());
+	System.out.println(myArray.findElement(3));
+	myArray.deleteByElement(5);
+	myArray.deleteByIndex(3);
+	myArray.addAtMiddle(1,1);
+		System.out.println(myArray.toString());
     }
 }
